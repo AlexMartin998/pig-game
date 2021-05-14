878d0065c00c7c09c3112fd98c749b08118ae1a4
AlexMartin998 <alextravc.342@gmail.com>
2021-05-14 16:19:17 -0500

    v4 commit

--- src/index.js
+++ src/index.js
@@ -9,21 +9,24 @@ const score0El = document.getElementById('score--0'),
 	btnRoll = document.querySelector('.btn--roll'),
 	btnHold = document.querySelector('.btn--hold'),
 	player0El = document.querySelector('.player--0'),
-    current0El = document.getElementById(`current--0`),
-    current1El = document.getElementById(`current--1`);
+	player1El = document.querySelector('.player--1'),
+    current0El = document.getElementById('current--0'),
+    current1El = document.getElementById('current--1');
+    
+    ;
 
 let scores, currentScore, activePlayer, playing;
 
 const init = () => {
 	score0El.textContent = 0;
 	score1El.textContent = 0;
-	diceEl.classList.add('hidden'),
-	scores = [0, 0],
-    currentScore = 0,
-    activePlayer = 0,
-    playing = true,
-    current0El = 0,
-    current1El = 0;
+	diceEl.classList.add('hidden');
+	scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
     player0El.classList.remove('player--winner');
     player1El.classList.remove('player--winner');
     player0El.classList.add('player--active');
